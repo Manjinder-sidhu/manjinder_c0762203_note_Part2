8a49ef4193f4187bcf0ec9d4d54ff44fbb7060af
Manjinder kaur <manusidhu181@gmail.com>
2019-11-09 21:07:18 -0500

    navigation title and background color settings

--- manjinder_c0762203_note_P1/foldersTableViewController.swift
+++ manjinder_c0762203_note_P1/foldersTableViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class foldersTableViewController: UITableViewController {
     var folders: [String]?
     var mytextfield = ""
+    
+    var scrollview: UIScrollView!
+    var containerview = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,10 +21,25 @@ class foldersTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         folders = [String]()
         
+        
+        self.tableView.backgroundColor =  . lightGray
         self.navigationItem.rightBarButtonItem = editButtonItem
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.black
+        
+         self.navigationController?.toolbar.barTintColor = UIColor.lightGray
+//        navigationController?.navigationBar.isTranslucent = true
+//        navigationController?.navigationBar.shadowImage = UIImage()
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        
+//        self.navigationItem.largeTitleDisplayMode =      self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+//       let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 1000))
+//       footerView.backgroundColor = bottomColor
+//       self.tableView.tableFooterView?.addSubview(footerView)
+//
+//        self.navigationController?.navigationBar.barTintColor = .lightGray
     }
 
   
@@ -31,28 +49,23 @@ class foldersTableViewController: UITableViewController {
     @IBAction func newfolderbtn(_ sender: Any) {
         let alertcontroller = UIAlertController(title: "New Folder", message: "Enter a name for this folder", preferredStyle: .alert)
         
-        
-//        let action = UIAlertAction(title: "Name Input", style: .default) { (alertAction) in
-//         textfield = alertcontroller.textFields![0] as UITextField
-//        }
-//
+    
     
         alertcontroller.addTextField { (textField ) in
                         textField.placeholder = "name"
             textField.text = ""
         }
-        let CancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        let CancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        CancelAction.setValue(UIColor.brown, forKey: "titleTextColor")
         let AddItemAction = UIAlertAction(title: "Add Item", style: .default){
-            
             (action) in
             let txtfield = alertcontroller.textFields![0]
-            
-           
             /// get the textfield instance form textFields array
             self.folders!.append(txtfield.text!)
             self.tableviewdata.reloadData()
             
         }
+       AddItemAction.setValue(UIColor.black, forKey: "titleTextColor")
                       alertcontroller.addAction(CancelAction)
                       alertcontroller.addAction(AddItemAction)
                       self.present(alertcontroller, animated: true, completion: nil)
@@ -79,6 +92,7 @@ class foldersTableViewController: UITableViewController {
                    let newitem = folders?[indexPath.row]
                     cell.textLabel?.text = newitem
                     cell.imageView?.image = UIImage(named: "folder-icon")
+                    cell.backgroundColor = .lightGray
                     return cell
            
             }
@@ -99,21 +113,16 @@ class foldersTableViewController: UITableViewController {
         return  .delete
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Folders"
-    }
     
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-       return 50
     
-    }
    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int){
-        view.tintColor = UIColor.white
+        view.tintColor = UIColor.lightGray
     }
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
+    
         return true
     }
    
@@ -130,7 +139,10 @@ class foldersTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
     }
    
-
+    
+    
+    
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
